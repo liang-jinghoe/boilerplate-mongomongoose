0ffaf7a407ace5a29ab0eaeca8c952b4c4c6473f
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 18:09:14 +0800

    Person schema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,19 @@ require('dotenv').config();
 let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  favouriteFoods: {
+    type: [String]
+  }
+});
+
+let Person = mongoose.modal("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
