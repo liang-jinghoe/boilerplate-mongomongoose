774934ef9f5b99cdae4967618679e720226f4c40
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 18:30:38 +0800

    fix mongo uri

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(`Connecing to ${process.env.MONGO_URI}`);
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String
