684d35c546ad1cd8bc24dbf525682aa737668e87
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-27 23:56:13 +0800

    Update findPeopleByName()

--- myApp.js
+++ myApp.js
@@ -64,7 +64,7 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { $set: { age: ageToSet } }, { new: true }, done);
 };
 
 const removeById = (personId, done) => {
